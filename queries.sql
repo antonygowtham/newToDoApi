1719b5d531d20ed21a3c9123b27e5c702bc8b8dc
antonygowtham <antonygowtham02@gmail.com>
2024-08-22 13:49:54 +0530

    documentation changes

--- queries.sql
+++ queries.sql
@@ -13,12 +13,3 @@ CREATE TABLE newtasks (
     is_finished BOOLEAN NOT NULL,
     user_id INTEGER REFERENCES newusers(id)
 );
-
-INSERT INTO newtasks (id, title, date, category, progress, is_finished, user_id)
-VALUES ('1', 'Task 1', '2024-06-27', 'Work', 50, true, 1);
-
-INSERT INTO newtasks (id, title, date, category, progress, is_finished, user_id)
-VALUES ('2', 'Task 2', '2024-06-28', 'Personal', 25, false, 1);
-
-INSERT INTO newtasks (id, title, date, category, progress, is_finished, user_id)
-VALUES ('3', 'Task 3', '2024-06-29', 'due', 25, false, 1);
